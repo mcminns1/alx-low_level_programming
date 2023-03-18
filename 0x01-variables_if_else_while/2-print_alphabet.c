9aff6ca8243227adb237fd64fb9ad32f59a25d43
Mcminn <mcminntheonly@gmail.com>
2023-03-17 17:43:05 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,23 +9,12 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int digit1, digit2;
+	char letter;
 
-	for (digit1 = 0; digit1 < 9; digit1++)
-	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-		{
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
 
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
-
-			if (digit1 == 8 && digit2 == 9)
-				continue;
-
-			putchar(',');
-			putchar(' ');
-		}
-	}
 	putchar('\n');
+
 	return (0);
 }
