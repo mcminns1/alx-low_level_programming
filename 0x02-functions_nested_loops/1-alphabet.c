d31f9b9c30aa131ed59b248857387ca815db7ccb
Mcminn <mcminntheonly@gmail.com>
2023-03-21 12:09:11 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+*/
+void print_alphabet(void);
+int main(void)
+{
+	print_alphabet();
+	putchar('\n');
+	return (0);
+}
+
+/**
+ * print_alphabet - loop
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char alphabets = 'a';
+	while (alphabets <= 'z')
+	{
+		putchar(alphabets);
+		alphabets++;
+	}
+}
