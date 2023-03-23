a157f2bb346280466084e7110b34f8b8fa213f7b
Mcminn <mcminntheonly@gmail.com>
2023-03-23 12:49:09 -0700

    1

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -24,7 +24,14 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-
+	else if (c > a && c > b)
+	{
+		largest = c;
+	}
+	else
+	{
+		largest = c;
+	}
 	return (largest);
 	putchar('\n');
 }
