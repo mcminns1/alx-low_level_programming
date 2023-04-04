18a903a39fd3b94ea3732a684e986d429329695c
Mcminn <mcminntheonly@gmail.com>
2023-04-04 04:35:52 -0700

    Up to date

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*_strchr - loates a character in a string
+*@s: the string to search
+*@c: the character to find
+*Return: first c occurance if c is found in string
+*	NULL if c is not found
+*/
+
+char *_strchr(char *s, char c)
+{
+	strchr(s, c);
+}
