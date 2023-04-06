f647904297af6ab1ab37f22dd27809bd0554b2fc
Mcminn <mcminntheonly@gmail.com>
2023-04-05 20:16:10 -0700

    Up to date

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,9 +7,9 @@
  * Return:int
  */
 
- int _pow_recursion(int x, int y)
- {
- 	if (y < 0)
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
 		return (-1);
 	else if (y != 0)
 		return (x * _pow_recursion(x, y - 1));
