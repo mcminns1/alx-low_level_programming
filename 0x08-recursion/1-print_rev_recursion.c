e34b8add8e1305dacc7f7da57ff5d85cd843a7ca
Mcminn <mcminntheonly@gmail.com>
2023-04-05 19:32:15 -0700

    Up to date

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
-*_print_rev_recursio: reverse
+*_print_rev_recursion - print a string in reverse
 *@s: pointer to string
+*return 0
 */
 
 void _print_rev_recursion(char *s)
