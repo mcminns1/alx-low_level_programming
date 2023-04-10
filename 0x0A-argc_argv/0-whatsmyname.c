efd1ef23138c6db519d990fe05ed501558b93857
Mcminn <mcminntheonly@gmail.com>
2023-04-10 16:13:12 -0700

    Whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,10 @@
 * return: 0 - success
 */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char **argv)
 {
+	(void)argc;
+
 	printf("%s\n", argv[0]);
 	return (0);
 }
