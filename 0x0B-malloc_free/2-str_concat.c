f7cccae6061c827b44a453d085c49a962dd8a111
Mcminn <mcminntheonly@gmail.com>
2023-04-11 16:54:47 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,14 +18,14 @@ char *str_concat(char *s1, char *s2)
 	if (s1 != NULL)
 	{
 		x = 0;
-		while (s1[x++] != '\0');
+		while (s1[x++] != '\0')
 			len1++;
 	}
 
 	if (s2 != NULL)
 	{
 		y = 0;
-		while (s2[y++] != '\0');
+		while (s2[y++] != '\0')
 			len2++;
 	}
 
