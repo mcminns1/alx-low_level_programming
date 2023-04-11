61d4f01786848a64cca3c25d924b0eb66b2eee00
Mcminn <mcminntheonly@gmail.com>
2023-04-11 16:43:43 -0700

    update

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,6 +2,7 @@
 
 void util(char **, char *);
 void create_word(char **, char *, int, int, int);
+
 /**
  * strtow - splits a string into words.
  * @str: the string
@@ -14,7 +15,7 @@ char **strtow(char *str)
 	int i, flag, len;
 	char **words;
 
-	if (str == NULL || str[0] == '\n' || (str[0] == ' ' & str[1] == '\0'))
+	if (str == NULL || str[0] == '\n' || (str[0] == ' ' && str[1] == '\0'))
 		return (NULL);
 
 	i = flag = len = 0;
