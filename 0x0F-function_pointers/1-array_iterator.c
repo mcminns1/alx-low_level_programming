7399e407600ce0d86a52a9a24ab84413fc035c5e
Mcminn <mcminntheonly@gmail.com>
2023-04-19 16:49:26 -0700

    1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,12 +10,12 @@
 
 void array_iterator(int array, size_t size, void (action)(int))
 {
-	int i;
-
-	if (!array || !action)
+	if (array == NULL || action == NULL)
 		return;
-	for (i = 0; i < (int) size; i++)
+
+	while (size--- > 0)
 	{
-		action(array[i]);
+		action(*array);
+		array++;
 	}
 }
