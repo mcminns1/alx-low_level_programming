e03d74fe5a7bea4c8fd8d0fdc849aa3d29b359ff
Mcminn <mcminntheonly@gmail.com>
2023-04-28 18:57:39 -0700

    100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include"lists.h"
+
+void first(void) __attribute__ ((constructor));
+/**
+* first - prints a sense befor e the main
+* function is executed
+*/
+
+void first(void)
+{
+	printf("You're the beat! and yet, you must also allow, \n");
+	printf("I bore my house upon my back!\n");
+}
