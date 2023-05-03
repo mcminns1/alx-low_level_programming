658fe6ce8eaa89e3db0ff50eb7e7593da45e7f4e
Mcminn <mcminntheonly@gmail.com>
2023-05-03 10:00:45 -0700

    update 1

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,8 +13,8 @@ size_t print_listint(const listint_t *h)
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-	//	h = h->next;
-	//	nnodes++;
+		current = h->next;
+		nnodes++;
 	}
 	return (nnodes);
 }
