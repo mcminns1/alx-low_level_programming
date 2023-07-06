127ae67783f0d26c26e96aba020bdcd5ce499bbe
Mcminn <mcminntheonly@gmail.com>
2023-07-06 16:45:52 -0700

    fix

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,16 +12,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i;
 
-	if (n == 0 && index < 64)
-		return (0);
-
-	for (i = 0; i <= 63; n >> 1, i++)
-	{
-		if (index == 1)
-		{
-			return (n & 1);
-		}
-	}
-
-	return (-1);
+	if (index >= 64)
+		return (-1);
+	return (((n >> index) & 1) ? 1 : 0);
 }
