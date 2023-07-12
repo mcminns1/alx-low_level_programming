0116699205c1635683d5b2be29601476b6e3c21b
Mcminn <mcminntheonly@gmail.com>
2023-07-12 12:13:32 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ size_t _strlen(char *str)
  * @text_content: NULL terinated string to add the end of the file
  * Return: 1 on success and -1 on failure
  */
-int appent_text_to_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t len;
