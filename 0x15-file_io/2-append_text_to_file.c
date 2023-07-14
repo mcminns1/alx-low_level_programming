7f446689ac84022e0700939df4388214758b4caf
Mcminn <mcminntheonly@gmail.com>
2023-07-14 15:26:02 -0700

    last

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,9 +18,10 @@ size_t _strlen(char *str)
 /**
  * append_text_to_file - appends a text at the end of a file
  * @filename: name of the file
- * @text_content: NULL terinated string to add the end of the file
+ * @text_content: NULL terminated string to add the end of the file
  * Return: 1 on success and -1 on failure
  */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
